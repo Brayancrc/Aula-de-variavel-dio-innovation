aaa12c2ffd1a8373d7bba640b385437d4da86f96
Brayan Robson <brayancrc@gmail.com>
2021-12-27 21:06:27 -0300

    first commit

---
+++ src/one/digital/innovation/aulavariavel/Main.java
@@ -0,0 +1,49 @@
+package one.digital.innovation.aulavariavel;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main<asrn24678nd, asrn2$4678_nd, i, I, _1a, $aq, quantidadeProduto, NUMERO_TENTATIVAS, QUANTIDADE_OPCOES, j> {
+
+    int i;
+    //int i;
+    int I;
+    //int 1a;
+    int  _1a;
+    int $aq;
+
+    i = 5;
+    I = 10;
+    _1a = 20;
+    $aq = 7;
+
+    final int j = 10;
+    //j= 15;
+    int asrn24678nd;
+    //int asrn246 78nd;
+    int asrn2$4678_nd = 10;
+    //int asrn2$46%78_nd; = 18;
+
+    asrn24678nd = 100;
+    asrn2$4678_nd = 10;
+
+    int quantidadeProduto = 50;
+    //int QuantidadeProduto;
+    final int NUMERO_TENTATIVAS = 5;
+    //final int numeroTentativas = 5;
+    int QUANTIDADE_OPCOES = 25;
+    //int qtdProd;
+
+    System.out.println(i);
+    System.out.println(I);
+    System.out.println(_1a);
+    System.out.println($aq);
+
+    System.out.println(j);
+    System.out.println(asrn24678nd);
+    System.out.println(asrn2$4678_nd);
+
+    System.out.println(quantidadeProduto);
+    System.out.println(NUMERO_TENTATIVAS);
+    System.out.println(QUANTIDADE_OPCOES);
+
+}
